6b4d86a1555c93249111355383f60838349a9a85
Dave <davidpcoffee@gmail.com>
2023-10-17 16:50:49 -0400

    Remove auto-dismiss

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,18 @@ import type { ActorSheetPF2e } from "@actor/sheet/base.js";
 import { registerSettingFlag } from "./settings.mts";
 import { preCreateItem_flagsItem } from "./flags/hooks.mts";
 import { preUpdateActor_SharedHp } from "./shared-hp/hooks.mts";
-import { updateActor_dismissEidolon } from "./auto-dismiss/hooks.mts";
 import { pf2eEndTurn_ForwardToEidolon, pf2eStartTurn_ForwardToEidolon } from "./encounters/hooks.mts";
 import { preUpdateItem_SubstituteSpellcastingBonus } from "./substitute-spell-stats/hooks.mts";
 import { renderActorSheet_LinkActor } from "./link-actors/hooks.mts";
 
 Hooks.once("init", () => {
   registerSettingFlag("shareSummonerHp", "Share Eidolon and Summoner HP");
-  registerSettingFlag("autoDismiss", "Remove Eidolon tokens at 0 HP");
   registerSettingFlag("eidolonCombatant", "Automate Eidolon's turn-based effects");
   registerSettingFlag("substituteSummonerSpellBonus", "Substitute Summoner's spellcasting bonus for Eidolon");
 });
 
 Hooks.on("preUpdateActor", async (actor, actorSource) => await preUpdateActor_SharedHp(actor as ActorPF2e, actorSource as DeepPartial<ActorSourcePF2e>));
 Hooks.on("preUpdateItem", async (item, itemSource) => await preUpdateItem_SubstituteSpellcastingBonus(item as ItemPF2e, itemSource as DeepPartial<ItemSourcePF2e>));
-Hooks.on("updateActor", async (actor, actorSource) => await updateActor_dismissEidolon(actor as ActorPF2e, actorSource as DeepPartial<ActorSourcePF2e>));
 Hooks.on("preCreateItem", async (item: unknown, itemSource: unknown) => await preCreateItem_flagsItem(item as ItemPF2e, itemSource as DeepPartial<ItemSourcePF2e>));
 Hooks.on("pf2e.startTurn", async (combatant, encounter) => await pf2eStartTurn_ForwardToEidolon(combatant as CombatantPF2e, encounter as EncounterPF2e));
 Hooks.on("pf2e.endTurn", async(combatant, encounter) => await pf2eEndTurn_ForwardToEidolon(combatant as CombatantPF2e, encounter as EncounterPF2e));
